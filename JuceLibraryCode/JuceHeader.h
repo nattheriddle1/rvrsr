2f185b6fa260ac35f025b3bebcb2c4db72c7870e
Nat Riddle <nriddle@choicesolutions.com>
2024-01-24 12:23:59 -0500

    Upgraded version number to 0.3.0

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -42,7 +42,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Rvrsr";
     const char* const  companyName    = "Nat Riddle";
-    const char* const  versionString  = "1.0.0";
-    const int          versionNumber  = 0x10000;
+    const char* const  versionString  = "0.3.0";
+    const int          versionNumber  = 0x300;
 }
 #endif
